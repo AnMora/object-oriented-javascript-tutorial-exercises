a8103a488322c914540c714214a7625de585a37b
Anmora96 <anmora.2396@gmail.com>
2021-02-05 17:45:06 +0000

    Ejercicio de practica

--- exercises/02-instance-of-datetime/app.js
+++ exercises/02-instance-of-datetime/app.js
@@ -1,2 +1,9 @@
-let d = new Date();
-console.log("Today is "+(d.getMonth()+1)+"-"+d.getDate()+"-"+d.getFullYear());
+let d = new Date(),
+    month = '' + (d.getMonth()+1),
+    day = '' + d.getDate(),
+    year = '' + d.getFullYear();
+
+    let _output = `Today is: ${month} - ${day} - ${year}`
+
+//console.log("Today is "+(d.getMonth()+1)+"-"+d.getDate()+"-"+d.getFullYear());
+console.log(_output);
--- exercises/06-Hello-world-with-classes/app.js
+++ exercises/06-Hello-world-with-classes/app.js
@@ -7,12 +7,17 @@ class Printer{
     	this._brand = 'Epson'; //will be a string 
     	this._model = 'TX-200'; //will be a string 
     }
-  
+    
     //Just like printers, this printer has a function that 
     //prints an example text to make sure the printer is working
     printerTest() { 
         return `My brand is ${this._brand} and my model ${this._model}`; 
-    } 
+    }
 }
 
 //Your code here
+
+let myPrinter = new Printer();
+let testResult = myPrinter.printerTest();
+
+console.log(testResult);
